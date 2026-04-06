0901706384e1d4d5842b2ef38435634a45315a62
Nivethika <nivaethika08@gmail.com>
2026-04-06 22:40:10 +0530

    donee bubble sort

---
+++ bubblesort.py
@@ -0,0 +1,7 @@
+arr=[int(x) for x in input().split()]
+for i in range(0,len(arr)-1):
+    for j in range(0,len(arr)-i-1):
+        if arr[j]>arr[j+1]:
+            arr[j],arr[j+1]=arr[j+1],arr[j]
+print(arr)
+
